88c39dbda151ee1446ff325a45458ca04faba6e2
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-23 23:57:47 +0200

    feat: create global error handler

--- app.ts
+++ app.ts
@@ -1,5 +1,19 @@
-import express, { Express } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
+import CustomError from "./util/custom-error";
 
-const app: Express = express()
+const app: Express = express();
 
-app.listen(5000, () => console.log("[server]: 5000"))
+app.use(
+  (
+    error: CustomError,
+    _request: Request,
+    response: Response,
+    _next: NextFunction
+  ) => {
+    const { message, errors, status } = error;
+
+    return response.status(status || 500).json({ message: errors || message });
+  }
+);
+
+app.listen(5000, () => console.log("[server]: 5000"));
