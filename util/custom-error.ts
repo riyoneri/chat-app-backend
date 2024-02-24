459480dd5b2969d203852f2961444f6393e51959
Lionel Kaneza <lionkaneza@gmail.com>
2024-02-24 10:49:37 +0200

    feat: create custom-error

---
+++ util/custom-error.ts
@@ -0,0 +1,9 @@
+export default class CustomError extends Error {
+  constructor(
+    public message: string,
+    public status?: number,
+    public errors?: { [key: string]: string }
+  ) {
+    super(message);
+  }
+}
