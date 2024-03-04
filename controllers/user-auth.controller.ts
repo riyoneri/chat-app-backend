0f24135cd3d9d59c3b32589df227ee4ab5f186a4
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-04 20:27:58 +0200

    feat: create login user controler

--- controllers/user-auth.controller.ts
+++ controllers/user-auth.controller.ts
@@ -1,10 +1,11 @@
-import { hashSync } from "bcryptjs";
+import { hashSync, compareSync } from "bcryptjs";
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import userModel from "../models/user.model";
 import CustomError from "../util/custom-error";
 import { deletefile } from "../util/file-system";
 import { serializeValidation } from "../util/validation";
+import { sign } from "jsonwebtoken";
 
 export const postRegister = async (
   request: Request,
@@ -17,7 +18,7 @@ export const postRegister = async (
     if (!errors.isEmpty()) {
       const error = new CustomError(
         "Validation failed",
-        422,
+        400,
         serializeValidation(errors),
       );
 
@@ -56,6 +57,76 @@ export const postRegister = async (
   } catch {
     const error = new CustomError("Internal server error");
     request.body.image && deletefile(request.body.image);
+    next(error);
+  }
+};
+
+export const postLogin = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  try {
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) {
+      const error = new CustomError(
+        "Incorrect E-mail address or password",
+        400,
+        serializeValidation(errors),
+      );
+
+      return next(error);
+    }
+
+    const user = await userModel.findOne({
+      $or: [
+        { email: request.body.emailOrUsername },
+        { username: request.body.emailOrUsername },
+      ],
+    });
+
+    if (!user) {
+      const error = new CustomError(
+        "Incorrect E-mail address or password",
+        400,
+      );
+
+      return next(error);
+    }
+
+    const passwordMatch = compareSync(request.body.password, user.password);
+
+    if (!passwordMatch) {
+      const error = new CustomError(
+        "Incorrect E-mail address or password",
+        400,
+      );
+
+      return next(error);
+    }
+
+    const token = sign({ id: user._id }, process.env.SECRET_KEY!, {
+      expiresIn: "1h",
+      noTimestamp: true,
+    });
+
+    const transformedUser = user.toJSON({
+      transform(document, returnValue) {
+        delete returnValue.password;
+        delete returnValue._id;
+
+        return {
+          _id: document._id,
+          ...returnValue,
+          imageUrl: `${request.protocol}://${request.headers.host}/${returnValue.imageUrl}`,
+        };
+      },
+    });
+
+    response.status(200).json({ user: transformedUser, token });
+  } catch {
+    const error = new CustomError("Internal server error");
     next(error);
   }
 };
