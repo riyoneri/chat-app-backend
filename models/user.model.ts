d44e48b56a456fa68bbcdcb94353fea2b0a170c7
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-04 17:31:51 +0200

    feat: create user model

---
+++ models/user.model.ts
@@ -0,0 +1,35 @@
+import { Document, Schema, model } from "mongoose";
+
+export interface IUserModel extends Document {
+  name: string;
+  email: string;
+  username: string;
+  imageUrl: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    image: { type: String, required: true },
+    password: { type: String, required: true },
+    createdAt: {
+      type: Date,
+      select: false,
+    },
+    updatedAt: {
+      type: Date,
+      select: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
+
+export default model<IUserModel>("User", userSchema);
