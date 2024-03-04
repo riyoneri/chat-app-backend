16025565cc078ab5f3cfa56f6db876d9c073326a
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-04 17:31:02 +0200

    feat: connect to mongodb

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import { config } from "dotenv";
 import express, { Express, NextFunction, Request, Response } from "express";
+import { connect } from "mongoose";
 import morgan from "morgan";
+import { exit } from "node:process";
 import userAuthRoutes from "./routes/user-auth.route";
 import CustomError from "./util/custom-error";
+config();
+
+const MONGODB_URL = process.env.MONGODB_URL;
 
 const app: Express = express();
 
@@ -26,4 +32,8 @@ app.use(
   },
 );
 
-app.listen(5000, () => console.log("[server]: 5000"));
+if (MONGODB_URL)
+  connect(MONGODB_URL)
+    .then(() => app.listen(5000, () => console.log("[server]: 5000")))
+    // eslint-disable-next-line unicorn/prefer-top-level-await
+    .catch(() => exit(1));
