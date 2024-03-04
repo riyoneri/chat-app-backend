cd231524b6831ee21534895fd445acc1ffdcc37a
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-04 18:51:52 +0200

    fix: update property in user model

--- models/user.model.ts
+++ models/user.model.ts
@@ -15,7 +15,7 @@ const userSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
-    image: { type: String, required: true },
+    imageUrl: { type: String, required: true },
     password: { type: String, required: true },
     createdAt: {
       type: Date,
