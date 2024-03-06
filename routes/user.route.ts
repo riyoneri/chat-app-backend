3e60971c406755809bc7b67fb539552e1c6534f2
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-06 11:45:57 +0200

    feat: protect get users route

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import * as userControllers from "../controllers/user.controller";
+import { isAuth } from "../middlewares/is-auth";
 
 const router = express.Router();
 
-router.get("/users", userControllers.getUsers);
+router.get("/users", isAuth, userControllers.getUsers);
 
 export default router;
