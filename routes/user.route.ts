90bf1cb8697ca66b2a9a09e408844d2cc552afdc
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-07 07:09:19 +0200

    refactor: add pages validation property

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import * as userControllers from "../controllers/user.controller";
 import { isAuth } from "../middlewares/is-auth";
+import { query } from "express-validator";
 
 const router = express.Router();
 
-router.get("/users", isAuth, userControllers.getUsers);
+router.get(
+  "/users",
+  isAuth,
+  [query("page").optional({ values: "undefined" }).toInt()],
+  userControllers.getUsers,
+);
 
 export default router;
