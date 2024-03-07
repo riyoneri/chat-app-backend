4ff33214d363c99f9dbb94f49950eeb7180cc5fd
Lionel Kaneza <lionkaneza@gmail.com>
2024-03-07 07:08:40 +0200

    feat: implement fetching users in chunks[pages]

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -2,15 +2,22 @@ import { NextFunction, Request, Response } from "express";
 import userModel from "../models/user.model";
 import CustomError from "../util/custom-error";
 
+const USERS_PER_PAGE = 1;
+
 export const getUsers = async (
   request: Request,
   response: Response,
   next: NextFunction,
 ) => {
   try {
+    const currentPage = Number(request.query.page) || 1;
+    const totalUsers = await userModel.countDocuments();
+
     const users = await userModel
       .find()
       .sort({ _id: -1 })
+      .skip((currentPage - 1) * USERS_PER_PAGE)
+      .limit(USERS_PER_PAGE)
       .transform((documents) =>
         documents.map((singleDocument) =>
           singleDocument.toJSON({
@@ -25,7 +32,10 @@ export const getUsers = async (
         ),
       );
 
-    response.json(users);
+    response.json({
+      users,
+      hasNextPage: USERS_PER_PAGE * currentPage < totalUsers,
+    });
   } catch {
     const error = new CustomError("Internal server error");
     next(error);
