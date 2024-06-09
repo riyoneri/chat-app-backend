7f2852cdf1eb4a9bfff12524a70df9f2dec72f4f
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-09 22:42:01 +0200

    refactor: change build script

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,5 @@ import express, { Request, Response } from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server: http://localhost:${port}`));
