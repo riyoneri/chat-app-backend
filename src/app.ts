94769ed15b35dd666f8527bdf3f6d538b566aeaf
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-10 22:23:19 +0200

    feat: format all files

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Request, Response } from "express";
+import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+for (const value of [1, 2, 3]) console.log("value");
 
 app.listen(port, () => console.log(`Server: http://localhost:${port}`));
