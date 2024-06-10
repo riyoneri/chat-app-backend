f3c4865968762e5548e407c3e1e5280aee016992
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-10 22:42:00 +0200

    chore: installl lint-staged packages

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,5 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-for (const value of [1, 2, 3]) console.log("value");
-
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server: http://localhost:${port}`));
