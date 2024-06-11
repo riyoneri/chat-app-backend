4c8b243c8ea3d9da8ff20727494d18ff656ed971
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-11 23:15:55 +0200

    chore: install rate-limiter-flexible package

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
-import express, { Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import helmet from "helmet";
+import { RateLimiterMemory } from "rate-limiter-flexible";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const rateLimiter = new RateLimiterMemory({
+  points: 6,
+  duration: 10,
+});
+
 app.use(helmet());
 app.disable("x-powered-by");
+
+app.use((request: Request, response: Response, next: NextFunction) => {
+  rateLimiter
+    .consume(request.ip!)
+    .then(() => {
+      next();
+    })
+    .catch(() => response.status(429).send("Too Many Requests"));
+});
 
 app.use((_, response: Response) => {
   response.status(404).send("URL does not exist");
