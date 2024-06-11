a2afb1ad93e7d2e3d358df9dc4f38f97ced1c4b9
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-11 21:43:18 +0200

    chore: install helmet package

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import helmet from "helmet";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(helmet());
+app.disable("x-powered-by");
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server: http://localhost:${port}`));
