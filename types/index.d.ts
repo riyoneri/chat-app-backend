6726f8c24d69e5bb92bd37d8480f2359703a07bc
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-17 13:14:42 +0200

    feat: delete all files

--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { IUserModel } from "../models/user.model";
-
-declare module "express-serve-static-core" {
-  interface Request {
-    user?: IUserModel;
-  }
-}
--- util/validation.ts
+++
@@ -1,9 +0,0 @@
-import { Result, ValidationError } from "express-validator";
-
-export const serializeValidation = (errors: Result<ValidationError>) => {
-  let mappedErrors = errors.mapped();
-  let refinedErrors: { [key: string]: string } = {};
-  for (let key in mappedErrors) refinedErrors[key] = mappedErrors[key]["msg"];
-
-  return refinedErrors;
-};
