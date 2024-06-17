f62ebc60e60e0163eba50002bb8da4350d783077
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-17 23:33:27 +0200

    feat: first steps of user registration

--- src/controllers/user-auth.controller.ts
+++ src/controllers/user-auth.controller.ts
@@ -1,14 +1,51 @@
+import { Upload } from "@aws-sdk/lib-storage";
+import { hashSync } from "bcrypt";
 import { NextFunction, Request, Response } from "express";
+import { nanoid } from "nanoid";
+import sharp from "sharp";
 
+import getClient from "../helpers/s3client";
+import customValidationResult from "../helpers/validation-results";
+import User from "../models/user.model";
 import CustomError from "../utils/custom-error";
 
 export const register = async (
-  _request: Request,
+  request: Request,
   response: Response,
   next: NextFunction,
 ) => {
   try {
-    response.status(500).json("Lion");
+    const validationResults = customValidationResult(request);
+    if (validationResults) {
+      const error = new CustomError("Validation error", 400, validationResults);
+
+      return next(error);
+    }
+
+    const resizedImage = await sharp(request.body.image)
+      .resize(1000, undefined, { fit: "cover" })
+      .toBuffer();
+
+    const { Key } = await new Upload({
+      client: getClient(),
+      params: {
+        Bucket: process.env.AWS_BUCKET_NAME!,
+        Key: `Xt8l/${nanoid(10)}.webp`,
+        Body: resizedImage,
+      },
+    }).done();
+
+    const hashedPassword = hashSync(request.body.password, 10);
+
+    const newUser = new User({
+      ...request.body,
+      password: hashedPassword,
+      imageUrl: Key,
+    });
+
+    const savedUser = await newUser.save();
+
+    response.status(201).json(savedUser.toJSON());
   } catch {
     const error = new CustomError("Internal server error.", 500);
     next(error);
