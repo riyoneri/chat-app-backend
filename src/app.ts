3e407f87c52dd81f2fa6f97c9b4bc6a6adb244ff
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-17 18:46:45 +0200

    feat: add limits on busboy config

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,20 @@ import busboy from "connect-busboy";
 import { config } from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import helmet from "helmet";
+import { connect } from "mongoose";
+import { exit } from "node:process";
 import { RateLimiterMemory } from "rate-limiter-flexible";
 
 import userAuthroute from "./routes/user-auth.route";
 import CustomError from "./utils/custom-error";
 
 config();
+
+const MONGODB_URL =
+  process.env.NODE_ENV === "development"
+    ? process.env.MONGODB_URL_LOCAL
+    : process.env.MONGODB_URL;
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,7 +40,7 @@ app.use((request: Request, response: Response, next: NextFunction) => {
     .catch(() => response.status(429).send("Too Many Requests"));
 });
 
-app.use(busboy({ limits: { files: 1 } }));
+app.use(busboy({ limits: { files: 1, fileSize: 1024 * 1024 * 4 } }));
 
 app.use("/auth/", userAuthroute);
 
@@ -48,5 +56,11 @@ app.use(
   },
 );
 
-// eslint-disable-next-line no-console
-app.listen(port, () => console.log(`Server: http://localhost:${port}`));
+if (MONGODB_URL)
+  connect(MONGODB_URL)
+    .then(() => {
+      // eslint-disable-next-line no-console
+      app.listen(port, () => console.log(`Server: http://localhost:${port}`));
+    })
+    // eslint-disable-next-line unicorn/prefer-top-level-await
+    .catch(() => exit(1));
--- src/utils/custom-error.ts
+++ src/utils/custom-error.ts
@@ -1,8 +1,12 @@
+import { Location } from "express-validator";
+
 export default class CustomError extends Error {
   constructor(
     public message: string,
     public statusCode?: number,
-    public errors?: { [key: string]: object },
+    public errors?: {
+      [key: string]: { location: Location; message: string };
+    }[],
   ) {
     super(message);
   }
