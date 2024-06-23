bcf8193028e5adfc3bbd22be5d8bd0a36ed8c508
Lionel Kaneza <lionkaneza@gmail.com>
2024-06-23 16:55:46 +0200

    refactor: update custom-error class

--- src/utils/custom-error.ts
+++ src/utils/custom-error.ts
@@ -1,12 +1,8 @@
-import { Location } from "express-validator";
-
 export default class CustomError extends Error {
   constructor(
     public message: string,
     public statusCode?: number,
-    public errors?: {
-      [key: string]: { location: Location; message: string };
-    }[],
+    public errors?: object,
   ) {
     super(message);
   }
